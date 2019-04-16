cc26754502d3abfe902085ee2ef8045377e84f8a
dnadan2 <_dan_@live.com>
2019-04-16 22:26:32 +0100

    61: more graphical changes

--- Items/scripts/Keys.gd
+++ Items/scripts/Keys.gd
@@ -2,7 +2,7 @@ class SilverKey extends "KeyBase.gd":
 	const texture = preload("res://assets/silver_key2.png")
 	
 	func _init():
-		iconFilePath = "res://assets/silver_key2.png"
+		iconFilePath = "res://assets/silver_key2.pgn"
 		description = "A Silver Key"
 		item_name = "Silver Key"
 		UnlockGuid = "Silver"
