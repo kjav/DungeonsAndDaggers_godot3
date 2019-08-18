688e333655e842b684b0ac29b330d162ba3e516f
dnadan2 <_dan_@live.com>
2019-08-18 16:43:59 +0100

    45: added a check for eating food

--- Items/scripts/Foods.gd
+++ Items/scripts/Foods.gd
@@ -8,6 +8,7 @@ class CookedSteak extends "Item.gd":
 	
 	func onUse():
 		#Audio.playSoundEffect(useSound, true)
-		.onUse()
-		GameData.player.heal(2)
-		GameData.foods.remove(GameData.foods.find(self))
+		if (GameData.player.stats.health.maximum > GameData.player.stats.health.value):
+			.onUse()
+			GameData.player.heal(2)
+			GameData.foods.remove(GameData.foods.find(self))
