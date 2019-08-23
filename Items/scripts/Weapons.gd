79802f16fe0c0ec92db7ec0e105262d3e2da6b1a
dnadan2 <_dan_@live.com>
2019-08-23 21:37:59 +0100

    101: bug fix

--- Items/scripts/Weapons.gd
+++ Items/scripts/Weapons.gd
@@ -1,26 +1,23 @@
 class BasicSword extends "Weapon.gd":
-	const texture = preload("res://assets/basic_sword.png")
-	
 	func _init():
 		iconFilePath = "res://assets/basic_sword.png"
+		texture = preload("res://assets/basic_sword.png")
 		item_name = "Basic Sword"
 		damage = 3
 		holdOffset = [Vector2(8.6666, 14), Vector2(24.3333, 12), Vector2(12, 14), Vector2(18.3333, 14)]
 
 class BasicShield extends "Weapon.gd":
-	const texture = preload("res://assets/basic_shield.png")
-	
 	func _init():
 		iconFilePath = "res://assets/basic_shield.png"
+		texture = preload("res://assets/basic_shield.png")
 		item_name = "Basic Shield"
 		damage = 1
 		holdOffset = [Vector2(21.3333, 18), Vector2(10.6666, 15), Vector2(16, 16), Vector2(16, 14)]
 
 class BasicSpear extends "Weapon.gd":
-	const texture = preload("res://assets/basic_spear.png")
-	
 	func _init():
 		iconFilePath = "res://assets/basic_spear.png"
+		texture = preload("res://assets/basic_spear.png")
 		item_name = "Basic Spear"
 		relativeAttackPositions = [Vector2(0, -1)]
 		damage = 1
