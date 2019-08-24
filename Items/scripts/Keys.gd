6727f3cf596c1105e52f65c3c54e715a900615dc
dnadan2 <_dan_@live.com>
2019-08-24 22:40:03 +0100

    103: bug fix

--- Items/scripts/Keys.gd
+++ Items/scripts/Keys.gd
@@ -1,8 +1,8 @@
 class SilverKey extends "KeyBase.gd":
-	const texture = preload("res://assets/silver_key2.png")
 	
 	func _init():
 		iconFilePath = "res://assets/silver_key2.pgn"
+		texture = preload("res://assets/silver_key2.png")
 		description = "A Silver Key"
 		item_name = "Silver Key"
 		UnlockGuid = "Silver"
