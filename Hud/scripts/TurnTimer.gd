5876bbb53c0ac5e83b224794efb4c63c574b346a
dnadan2 <_dan_@live.com>
2019-08-25 13:25:52 +0100

    80: refactor

--- Hud/scripts/TurnTimer.gd
+++ Hud/scripts/TurnTimer.gd
@@ -1,9 +1,13 @@
 extends Node2D
 
+const timePerTurn = 1
+var timerNode
+
 func _ready():
 	GameData.player.connect("turnTimeChange", self, "timeChange")
+	timerNode = self.get_node("Turn Timer")
 
-func timeChange(time_elapsed):
-	var desiredFrame = floor( time_elapsed / 1 * 20 )
+func timeChange(timeElapsed):
+	var desiredFrame = floor( timeElapsed / timePerTurn * (timerNode.frames.get_frame_count("default") - 1) )
 	
-	self.get_node("Turn Timer").set_frame(desiredFrame)
+	timerNode.set_frame(desiredFrame)
