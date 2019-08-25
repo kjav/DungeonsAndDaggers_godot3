e0c9acbb548e0a77b37ccc6aaf8850217d66eaa2
dnadan2 <_dan_@live.com>
2019-08-25 22:03:26 +0100

    30: bug fix and some refactoring

--- Hud/scripts/HudButtonClick.gd
+++ Hud/scripts/HudButtonClick.gd
@@ -6,6 +6,7 @@ func _pressed():
         #Audio.playSoundEffect("Inventory_Open",true)
         var new_inventory_instance = InventoryType.instance()
         new_inventory_instance.set_position(Vector2(97, 245))
+        new_inventory_instance.get_node("ClickableRegion").set_position(Vector2(97, 245))
         new_inventory_instance.setType(self.get_parent().getType())  
         new_inventory_instance.set_name("Inventory")
         get_tree().get_current_scene().get_node("HudNode").get_node("HudCanvasLayer").add_child(new_inventory_instance)
