e21ae75659246384090a6c3b7108ff3e200684e5
Sam <sam.rr.lee@gmail.com>
2019-09-01 13:29:01 +0100

    Added stair graphics and inserted randomly to test

---
+++ Characters/scripts/Boss.gd
@@ -0,0 +1,6 @@
+extends "Enemy.gd"
+
+func handleCharacterDeath():
+	# TODO: Create stairs environment
+	# TODO: Change how boss items are dropped so they're not on the stairs
+	.handleCharacterDeath()
