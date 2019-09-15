668005b90257a32750cf9af3fca4f7d17cb54c77
dnadan2 <_dan_@live.com>
2019-09-15 19:06:33 +0100

    131: reset firespirit to direct process

--- Characters/scripts/FireSpirit.gd
+++ Characters/scripts/FireSpirit.gd
@@ -5,7 +5,7 @@ const Process = preload("res://Characters/scripts/behaviours/_Process.gd")
 
 func _ready():
 	turnBehaviour = Turn.MoveUpRightDownLeft.new()
-	processBehaviour = Process.Bounce.new()
+	processBehaviour = Process.Direct.new()
 	self.character_name = 'Fire Spirit'
 	base_damage = 2
 	item_distribution = Constants.IndependentDistribution.new([{"p": 1, "value": Constants.SpellClasses.FireSpell}])
