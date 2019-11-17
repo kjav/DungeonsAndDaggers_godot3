c17d88efcdf1114b3ec30c7e8141b5fd1038b76d
dnadan2 <_dan_@live.com>
2019-11-17 00:24:13 +0000

    166: made buttons clearer when disabled

--- Hud/scripts/HudWeaponButtons.gd
+++ Hud/scripts/HudWeaponButtons.gd
@@ -1,8 +1,8 @@
 extends Node2D
 
 func _ready():
-	get_node("Primary Inactive Ovelay").representedSlot = Enums.WEAPONSLOT.PRIMARY
-	get_node("Secondary Inactive Ovelay").representedSlot = Enums.WEAPONSLOT.SECONDARY
+	get_node("Primary Inactive Overlay").representedSlot = Enums.WEAPONSLOT.PRIMARY
+	get_node("Secondary Inactive Overlay").representedSlot = Enums.WEAPONSLOT.SECONDARY
 
 func setIconTexture(slot, texture):
     if slot == Enums.WEAPONSLOT.PRIMARY:
@@ -11,5 +11,5 @@ func setIconTexture(slot, texture):
 	    get_node("Secondary Icon").set_texture(texture)
 
 func SetCurrentWeapon(slot):
-	get_node("Primary Inactive Ovelay").visible = not slot == Enums.WEAPONSLOT.PRIMARY
-	get_node("Secondary Inactive Ovelay").visible = not slot == Enums.WEAPONSLOT.SECONDARY
+	get_node("Primary Inactive Overlay").visible = not slot == Enums.WEAPONSLOT.PRIMARY
+	get_node("Secondary Inactive Overlay").visible = not slot == Enums.WEAPONSLOT.SECONDARY
