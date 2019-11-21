b3b3628d9c6ccabfdcc209719ce6f6c5d392252d
dnadan2 <_dan_@live.com>
2019-11-21 22:57:19 +0000

    171: inventory restyling

--- Hud/scripts/HudButtonClick.gd
+++ Hud/scripts/HudButtonClick.gd
@@ -5,8 +5,6 @@ func _pressed():
     if not get_tree().get_current_scene().get_node("HudNode").inventoryOpen:
         #Audio.playSoundEffect("Inventory_Open",true)
         var new_inventory_instance = InventoryType.instance()
-        new_inventory_instance.set_position(Vector2(97, 245))
-        new_inventory_instance.get_node("ClickableRegion").set_position(Vector2(97, 245))
         new_inventory_instance.setType(self.get_parent().getType())  
         new_inventory_instance.set_name("Inventory")
         get_tree().get_current_scene().get_node("HudNode").get_node("HudCanvasLayer").add_child(new_inventory_instance)
