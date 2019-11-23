3963a1314c86b5fb8b50b4e87b9443b801e81934
dnadan2 <_dan_@live.com>
2019-11-23 12:01:48 +0000

    171: inventory now populates based on what button was clicked and items

--- Hud/scripts/InventoryItemDisplay.gd
+++ Hud/scripts/InventoryItemDisplay.gd
@@ -1,13 +1,10 @@
 extends Node2D
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var instance
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func setInstance(newInstance):
+	instance = newInstance
+	#get_node("ItemIcon/Icon").set_texture(instance.texture)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func getInstance():
+	return instance
