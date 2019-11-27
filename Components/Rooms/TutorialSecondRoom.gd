d9993f46f1856997c0b6dc01acbf6dd54d4deabd
dnadan2 <_dan_@live.com>
2019-11-27 21:37:33 +0000

    37: added water spirit

--- Components/Rooms/TutorialSecondRoom.gd
+++ Components/Rooms/TutorialSecondRoom.gd
@@ -1,11 +1,33 @@
 extends 'RoomBase.gd'
 
 func setup_params():
-	extents_distribution = Set.new([Vector2(6, 9)])
-	npc_distribution = Distribution.new([])
+	extents_distribution = Set.new([Vector2(5, 10)])
+	npc_distribution =  IndependentDistribution.new(
+	[
+		{
+			"p": 1, 
+			"value": load("res://Characters/WaterSpirit.tscn"),
+			"position": Vector2(2, 2)
+		},
+		{
+			"p": 1, 
+			"value": load("res://Characters/FireSpirit.tscn"),
+			"position": Vector2(2, 3)
+		},
+		{
+			"p": 1, 
+			"value": load("res://Characters/WaterSpirit.tscn"),
+			"position": Vector2(2, 5)
+		},
+		{
+			"p": 1, 
+			"value": load("res://Characters/FireSpirit.tscn"),
+			"position": Vector2(2, 6)
+		}
+	])
 	environment_distribution = Distribution.new([{
 		"p": 1,
 		"value": load("res://Environments/LevelStairs.tscn"),
-		"position": Vector2(3, 6)
+		"position": Vector2(3, 2)
 	}])
 	item_distribution = Distribution.new([])
