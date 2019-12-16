65a461cd9cc01e6d9d41a0dcd4de9ef370e2e0a9
Sam <sam.rr.lee@gmail.com>
2019-12-16 01:26:00 +0000

    Deleted old files

--- Items/scripts/_Keys.gd
+++
@@ -1,8 +0,0 @@
-class SilverKey extends "KeyBase.gd":
-	
-	func _init():
-		iconFilePath = "res://assets/silver_key2.pgn"
-		texture = preload("res://assets/silver_key2.png")
-		description = "A Silver Key"
-		item_name = "Silver Key"
-		UnlockGuid = "Silver"
