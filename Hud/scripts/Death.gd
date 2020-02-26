ac848f5ef284b67032a71eed50a05ee0160e4b77
dnadan2 <_dan_@live.com>
2020-02-26 21:55:36 +0000

    226: restyled and refactored

--- Hud/scripts/Death.gd
+++ Hud/scripts/Death.gd
@@ -2,4 +2,3 @@ extends Control
 
 func died():
 	show()
-	get_node("PlaythroughStats").SetLabels(GameData.player_kills, GameData.current_level)
--- Hud/scripts/PlaythroughStats.gd
+++ Hud/scripts/PlaythroughStats.gd
@@ -1,15 +1,5 @@
 extends Node2D
 
-const monsterText = "Monsters Defeated: "
-const levelText = "Level Reached: "
-# const upgradesText = "Upgrades Learnt: "
-# const chestsText = "Chests Looted: "
-# const slainText = "Slain By A "
-const outOfText = " / "
-
-func SetLabels(kills, level):
-	get_node("Monsters").text = monsterText + str(kills) + outOfText + "18"
-	get_node("Level").text = levelText + str(level) + outOfText + "4"
-	# get_node("Upgrades").text = upgradesText + str(upgrades) + outOfText + "5"
-	# get_node("Chests").text = chestsText + str(chests) + outOfText + "2"
-	# get_node("Slain").text = slainText + slainBy
+func _ready():
+	get_node("Monsters Amount").text = str(GameData.player_kills)
+	get_node("Level Amount").text = str(GameData.current_level)
