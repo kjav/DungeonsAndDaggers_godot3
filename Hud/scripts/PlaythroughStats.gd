874d9aeabbdce4eb5b8e9546dfdcdc11788a61d5
dnadan2 <_dan_@live.com>
2020-03-03 21:28:07 +0000

    226: highscore text

--- Hud/scripts/PlaythroughStats.gd
+++ Hud/scripts/PlaythroughStats.gd
@@ -3,28 +3,34 @@ extends Node2D
 var record_player_kills = 0
 var record_current_level = 0
 
-func _ready():
-	load_highscores()
-
 func _draw():
+	load_highscores()
 	get_node("Monsters Amount").text = str(GameData.player_kills)
-	var playerKillsRecord = max(record_player_kills, GameData.player_kills)
-	get_node("Monsters Highscore Amount").text = str(playerKillsRecord)
+	get_node("Monsters Highscore Amount").text = str(record_player_kills)
+	
+	if (record_player_kills < GameData.player_kills):
+		get_node("Monsters New Highscore").show()
+		get_node("Monsters Highscore Text").hide()
+		get_node("Monsters Highscore Amount").add_color_override("font_color", Color(0.5,0.5,0.5,1))
 
 	get_node("Level Amount").text = str(GameData.current_level)
-	var levelRecord = max(record_current_level, GameData.current_level)
-	get_node("Level Highscore Amount").text = str(levelRecord)
-
-	save_highscores(playerKillsRecord, levelRecord)
+	get_node("Level Highscore Amount").text = str(record_current_level)
 	
-func save_highscores(killsHighscore, levelHighscore):
+	if (record_current_level < GameData.current_level):
+		get_node("Level New Highscore").show()
+		get_node("Level Highscore Text").hide()
+		get_node("Level Highscore Amount").add_color_override("font_color", Color(0.5,0.5,0.5,1))
+
+	save_highscores()
+	
+func save_highscores():
 	var highscores = File.new()
 	
 	highscores.open(highscores_save_name(), File.WRITE)
 	
 	highscores.store_line(to_json({
-		"kills": killsHighscore,
-		"level": levelHighscore
+		"kills": max(record_player_kills, GameData.player_kills),
+		"level": max(record_current_level, GameData.current_level)
 	}))
 
 	highscores.close()
