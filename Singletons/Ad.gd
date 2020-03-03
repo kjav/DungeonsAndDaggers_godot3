9a0cb22d8f006bf0f7a8e9be4aa4c71d823b7ba6
Sam <sam.rr.lee@gmail.com>
2020-03-03 21:50:14 +0000

    Added test reward ad

---
+++ Singletons/Ad.gd
@@ -0,0 +1,21 @@
+extends Node
+var admob = null
+var isReal = false
+
+var adRewardId = "ca-app-pub-6580148569317237/9855207822"
+
+func _ready():
+	call_deferred("_init_ads")
+
+func _init_ads():
+	if(Engine.has_singleton("AdMob")):
+		admob = Engine.get_singleton("AdMob")
+		var res = admob.init(isReal, get_instance_id())
+
+		admob.connect("_on_rewarded", self, "on_rewarded")
+		admob.resize()
+		admob.loadRewardedVideo(adRewardId)
+		admob.showRewardedVideo()
+
+func on_rewarded(currency, amount):
+	print("Rewarded ", amount, " ", currency)
