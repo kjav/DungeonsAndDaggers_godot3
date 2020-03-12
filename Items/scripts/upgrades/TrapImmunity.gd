7417ca8d9d107412cd26f929fd6f138afc714bba
dnadan2 <_dan_@live.com>
2020-03-12 23:02:55 +0000

    224: reword

--- Items/scripts/upgrades/TrapImmunity.gd
+++ Items/scripts/upgrades/TrapImmunity.gd
@@ -2,7 +2,7 @@ extends "UpgradeBase.gd"
 
 func _init():
 	texture = preload("res://assets/31_a.png")
-	description="Become immune to all traps, you will be able to walk over spike traps unharmed.\n +Trap Immune"
+	description="Become immune to all traps, you will be able to walk over spike traps unharmed.\n\n   +Trap Immune"
 	title="Trap Immunity"
 
 func onUse():
