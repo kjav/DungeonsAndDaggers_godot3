f29cbe269ac0d98f5d3fc32115ab1f7544605125
Sam <sam.rr.lee@gmail.com>
2020-03-12 00:00:31 +0000

    Added load animation to video

---
+++ Hud/Trophy.gd
@@ -0,0 +1,10 @@
+extends TextureRect
+
+export(bool) var grayscale setget set_grayscale, get_grayscale
+
+func set_grayscale(val):
+	print(val)
+	material.set_shader_param("grayscale", val)
+
+func get_grayscale():
+	return material.get_shader_param("grayscale")
