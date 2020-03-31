aef6ba199ae0a0510beb9a4da505a14403c8ff75
Sam <sam.rr.lee@gmail.com>
2020-03-31 20:50:33 +0100

    Removed new lines from ends of files (godot 3.2.1 update)

--- Hud/scripts/HudButtonClick.gd
+++ Hud/scripts/HudButtonClick.gd
@@ -2,11 +2,11 @@ extends TextureButton
 const InventoryType = preload("res://Hud/Inventory.tscn")
 
 func _pressed():
-    if not get_tree().get_current_scene().get_node("HudNode").inventoryOpen:
-        #Audio.playSoundEffect("Inventory_Open",true)
-        get_tree().get_current_scene().get_node("HudNode").inventoryOpen = true
-        var new_inventory_instance = InventoryType.instance()
-        new_inventory_instance.setType(self.get_parent().getType())  
-        new_inventory_instance.set_name("Inventory")
-        get_tree().get_current_scene().get_node("HudNode").get_node("HudCanvasLayer").add_child(new_inventory_instance)
-        new_inventory_instance.populateInventory()
+	if not get_tree().get_current_scene().get_node("HudNode").inventoryOpen:
+		#Audio.playSoundEffect("Inventory_Open",true)
+		get_tree().get_current_scene().get_node("HudNode").inventoryOpen = true
+		var new_inventory_instance = InventoryType.instance()
+		new_inventory_instance.setType(self.get_parent().getType())  
+		new_inventory_instance.set_name("Inventory")
+		get_tree().get_current_scene().get_node("HudNode").get_node("HudCanvasLayer").add_child(new_inventory_instance)
+		new_inventory_instance.populateInventory()
