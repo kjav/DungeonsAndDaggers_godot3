374bf090662d5a1c35d354c12946961d21addfaa
dnadan2 <_dan_@live.com>
2020-04-19 16:08:02 +0100

    fixed inventory one and put into a shared base class

--- Hud/scripts/InventoryItemClick.gd
+++ Hud/scripts/InventoryItemClick.gd
@@ -1,34 +1,12 @@
-extends TextureButton
+extends "ItemPopupBaseScript.gd"
 
-const ItemDescriptionPopup = preload("res://Hud/ItemDescriptionPopup.tscn")
-var popupAdded = true
-var pressStartTime = OS.get_ticks_msec()
-var mousePosition
+func actionShortPress():
+	var parent = self.get_parent()
+	
+	parent.queue_free()
+	parent.hide()
+	parent.get_parent().selectItem(parent)
+	parent.instance.onUse()
 
-func _input(event):
-	if event.is_action_pressed("click"):
-		pressStartTime = OS.get_ticks_msec()
-		popupAdded = false
-		mousePosition = event.position
-	elif event.is_action_released("click"):
-		if popupAdded:
-			get_tree().get_current_scene().get_node("HudNode").get_node("HudCanvasLayer").get_node("ItemDescriptionPopup").hidePopup()
-		elif !isLongPress():
-				var parent = self.get_parent()
-				
-				parent.queue_free()
-				parent.hide()
-				parent.get_parent().selectItem(parent)
-				parent.instance.onUse()
-
-func _process(delta):
-	if Input.is_mouse_button_pressed(BUTTON_LEFT) and isLongPress() and !popupAdded:
-			var new_instance = ItemDescriptionPopup.instance()
-			popupAdded = true
-			new_instance.set_name("ItemDescriptionPopup")
-			new_instance.setItem(get_parent().getInstance())
-			new_instance.setPopupPosition(mousePosition)
-			get_tree().get_current_scene().get_node("HudNode").get_node("HudCanvasLayer").add_child(new_instance)
-
-func isLongPress():
-	return (OS.get_ticks_msec() - pressStartTime) > 100
+func getItem():
+	return get_parent().getInstance()
---
+++ Hud/scripts/ItemPopupBaseScript.gd
@@ -0,0 +1,50 @@
+extends TextureButton
+
+const ItemDescriptionPopup = preload("res://Hud/ItemDescriptionPopup.tscn")
+var popupAdded = false
+var clickInProgress = false
+var pressStartTime = OS.get_ticks_msec()
+var mousePosition = Vector2(9999,9999)
+
+func actionShortPress():
+	pass
+
+func getItem():
+	pass
+
+func _input(event):
+	if !clickInProgress and event.is_action_pressed("click") and withinTileBounds(event.position):
+		pressStartTime = OS.get_ticks_msec()
+		popupAdded = false
+		clickInProgress = true
+		mousePosition = event.position
+	elif clickInProgress and event.is_action_released("click") and withinTileBounds(mousePosition):
+		if popupAdded:
+			popupAdded = false
+			clickInProgress = false
+			mousePosition = Vector2(9999,9999)
+			
+			for node in GameData.hud.get_node("HudCanvasLayer/Popups").get_children():
+		        node.hidePopup()
+		elif !isLongPress():
+			actionShortPress()
+			popupAdded = false
+			clickInProgress = false
+
+func _process(delta):
+	if clickInProgress and withinTileBounds(mousePosition) and Input.is_mouse_button_pressed(BUTTON_LEFT) and isLongPress() and !popupAdded:
+
+			var new_instance = ItemDescriptionPopup.instance()
+			popupAdded = true
+			new_instance.set_name("ItemDescriptionPopup")
+			new_instance.setItem(getItem())
+			new_instance.setPopupPosition(mousePosition)
+			GameData.hud.get_node("HudCanvasLayer/Popups").add_child(new_instance)
+
+func isLongPress():
+	return (OS.get_ticks_msec() - pressStartTime) > 100
+	
+func withinTileBounds(pos):
+	var size = self.get_global_transform().get_scale() * self.get_size()
+	
+	return pos.x > self.get_global_position().x and pos.x < self.get_global_position().x + size.x and pos.y > self.get_global_position().y and pos.y < self.get_global_position().y + size.y
