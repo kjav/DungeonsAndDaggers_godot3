171ce41d3d14e03bd9a54199348d3952f892797e
dnadan2 <_dan_@live.com>
2020-05-03 16:15:20 +0100

    player effect icons

--- Hud/scripts/PlayerEffect.gd
+++ Hud/scripts/PlayerEffect.gd
@@ -1,10 +1,18 @@
 extends "TimerBase.gd"
 
-var icon
 var effectName
 var effectDescription
 var proportion
 
+func _init():
+	colour = Color(1,1,1)
+
 func setProportion(_proportion):
 	proportion = _proportion
+	
+	degrees = 1 * 360
+	
 	update()
+
+func setEffect(effect):
+	get_node("TextureRect").texture = effect.texture
