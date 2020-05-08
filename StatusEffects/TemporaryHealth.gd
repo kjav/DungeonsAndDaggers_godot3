6d6e5e34d02182378aaaaed2ae9d2bb384aaa307
dnadan2 <_dan_@live.com>
2020-05-08 15:42:32 +0100

    minor tweeks

--- StatusEffects/TemporaryHealth.gd
+++ StatusEffects/TemporaryHealth.gd
@@ -3,4 +3,4 @@ extends "StatusEffectBase.gd"
 func _init():
 	texture = preload("res://assets/red_simple_potion.webp")
 	effectName = "Temporary Health"
-	effectDescription = "You have extra temporary health for the duration of this effect. If any of the temporary health remains by the end it will be removed."
+	effectDescription = "You have extra health for the duration of this effect. If any of the that health remains by the end it will be removed."
