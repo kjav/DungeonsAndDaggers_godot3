7abfc26b5e18e90037a6fe83f24e0cdeb6266d95
dnadan2 <_dan_@live.com>
2020-06-20 00:01:58 +0100

    fixed double boss room dropping stairs individually

--- Characters/scripts/Boss.gd
+++ Characters/scripts/Boss.gd
@@ -3,10 +3,23 @@ extends "Enemy.gd"
 const Stairs = preload("res://Environments/LevelStairs.tscn")
 
 func handleCharacterDeath():
-	var Environments = self.get_node("/root/Node2D/Environments")
-	var node = Stairs.instance()
-	node.set_position(position)
-	Environments.add_child(node)
-	GameData.environmentObjects.append(node)
+	var anyRemainingBosses = anyOtherBossesRemaining()
+	
+	if !anyRemainingBosses:
+		var Environments = self.get_node("/root/Node2D/Environments")
+		var node = Stairs.instance()
+		node.set_position(position)
+		Environments.add_child(node)
+		GameData.environmentObjects.append(node)
+	
 	# TODO: Change how boss items are dropped so they're not on the stairs
 	.handleCharacterDeath()
+
+func anyOtherBossesRemaining():
+	var a = load(get_script().get_path())
+	for character in GameData.characters:
+		if character != self:
+			if character is a:
+				return true
+	
+	return false
