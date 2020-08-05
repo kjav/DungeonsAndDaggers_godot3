ca7858e76c5841f905419e8ccf6bff61ec786875
dnadan2 <_dan_@live.com>
2020-08-05 17:00:02 +0100

    fixed some items lingering issue (if more than one of the same item on same tile)

--- Items/FloorItems.gd
+++ Items/FloorItems.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+var associatedSprites = {}
+
 func _ready():
 	for i in GameData.placedItems:
 		addItem(i)
@@ -10,12 +12,11 @@ func setupConnection():
 	GameData.player.connect("itemPickedUp", self, "remove")
 
 func remove(item):
-	for i in get_children(): 
-		#might get wrong one if two of same type in the same place
-		if (i.texture == item.texture and i.get_position() == item.position + Vector2(64,64)):
-			i.hide()
-			i.queue_free()
-			break
+	var sprite = associatedSprites[item.get_instance_id()]
+	
+	if sprite != null:
+		sprite.hide()
+		sprite.queue_free()
 
 func addItem(newItem):
 	var s = Sprite.new()
@@ -23,4 +24,5 @@ func addItem(newItem):
 	s.set_scale(Vector2(0.5, 0.5))
 	s.set_position(newItem.position + Vector2(64,64))
 	add_child(s)
+	associatedSprites[newItem.get_instance_id()] = s
 
